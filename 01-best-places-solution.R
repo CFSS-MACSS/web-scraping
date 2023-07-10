ea951b54ba1b62d67ae7b4f03b8c8d311b152031
Jean Clipperton <41075491+jmclip@users.noreply.github.com>
2023-07-10 07:47:32 -0700

    Update 01-best-places-solution.R

--- 01-best-places-solution.R
+++ 01-best-places-solution.R
@@ -2,21 +2,21 @@ library(tidyverse)
 library(rvest)
 
 ## SPERLING'S BEST PLACES 
-# SOLUTION CODE USING STERLING, VIRGINA AS A DEMO
+# SOLUTION CODE USING DES MOINES, IOWA AS A DEMO
 
 # TASK 1: Look up the cost of living for your hometown on 
 # Sperling's Best Places website: http://www.bestplaces.net/ 
 # Then, extract it with `html_elements()` and `html_text2()`
 
 # download the HTML page
-sterling <- read_html("http://www.bestplaces.net/cost_of_living/city/virginia/sterling")
+dsm <- read_html("https://www.bestplaces.net/cost_of_living/city/iowa/des_moines")
 
 # extract the cost of living value
-col <- html_elements(sterling, css = "#mainContent_dgCostOfLiving tr:nth-child(2) td:nth-child(2)")
+col <- html_elements(dsm, css = "#mainContent_dgCostOfLiving tr:nth-child(2) td:nth-child(2)")
 html_text2(col)
 
 # do the same thing using a piped operation
-sterling %>%
+dsm %>%
   html_elements(css = "#mainContent_dgCostOfLiving tr:nth-child(2) td:nth-child(2)") %>%
   html_text2()
 
@@ -25,27 +25,16 @@ sterling %>%
 # To exact tables use the rvest function html_table()
 
 # extract the first table
-tables <- html_elements(sterling, css = "table")
+tables <- html_elements(dsm, css = "table")
 html_table(tables, header = TRUE, fill = TRUE)[[1]]
 
-# do the same thing using a piped operation
-tables %>%
-  # get the first table using nth() function
-  nth(1) %>%
-  # convert to data frame
-  html_table(header = TRUE)
 
 
 # TASK 3: Extract the climate statistics table of your hometown
 
-sterling_climate <- read_html("http://www.bestplaces.net/climate/city/virginia/sterling")
+dsm_climate <- read_html("http://www.bestplaces.net/climate/city/iowa/des_moines")
 
 # extract the climate table and put it into a data frame
-climate <- html_elements(sterling_climate, css = "table")
+climate <- html_elements(dsm_climate, css = "table")
 html_table(climate, header = TRUE, fill = TRUE)[[1]]
 
-# do the same thing using a piped operation
-sterling_climate %>%
-  html_elements(css = "table") %>%
-  nth(1) %>%
-  html_table(header = TRUE)
